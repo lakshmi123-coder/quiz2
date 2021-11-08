10a3379d4d4ffe0b222e132e8f82d5b109777486
lakshmi123-coder <87013273+lakshmi123-coder@users.noreply.github.com>
2021-11-08 20:55:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  database = firebase.database();
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+//call the play function
+gameState.play()
+  }
+}
